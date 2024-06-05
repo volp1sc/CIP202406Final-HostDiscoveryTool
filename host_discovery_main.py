772293621fb5b2f64d874308e8455d81d20880a5
volp1sc <volp1s.mail@gmail.com>
2024-06-05 21:45:24 +0200

    First Project Commit

---
+++ host_discovery_main.py
@@ -0,0 +1,12 @@
+from user_input import user_input
+
+def main():
+    # get IP range
+    answ = user_input()
+    print(answ)
+
+
+# print('INIZIO')
+if __name__ == '__main__':
+    main()
+# print('FINE')
---
+++ user_input.py
@@ -0,0 +1,7 @@
+# return user input as IP range inside a list
+# acceptable are two IP in dotted notation
+# or an IP and a gateway mask
+
+def user_input():
+    choice = ['192.168.0.240']  # '192.168.0.240/32'
+    return choice
