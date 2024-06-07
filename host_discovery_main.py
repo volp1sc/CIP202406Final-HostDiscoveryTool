26f8b7d98622368a3c8fa111b1c2ae5342cea464
volp1sc <volp1s.mail@gmail.com>
2024-06-07 18:06:06 +0200

    working on IP mathematic still some issues

--- host_discovery_main.py
+++ host_discovery_main.py
@@ -1,10 +1,18 @@
-from user_input import user_input
+# from user_input import user_input
+from user_input import user_input, bit, decode
 
-def main():
+def main1():
     # get IP range
     answ = user_input()
     print(answ)
 
+def main():
+    ip = [192,168,0,240]
+    nip = ip.copy()
+    bt = int(input("Range ? "))
+    print(ip, nip, bt)
+    bit(ip,nip,bt)
+    print(ip, nip, bt)
 
 # print('INIZIO')
 if __name__ == '__main__':
--- user_input.py
+++ user_input.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+import math
 
 def get_interface_ip(interface):
     """Get IP address for a given network interface"""
@@ -45,26 +46,102 @@ def ip_input():
             ddd = int_value 
     return [aaa, bbb, ccc, ddd]
 
-def nm_input(starting,flag):
+def decode(nm_val):
+    if nm_val > 0 :
+        print("decode nm_val", nm_val, float(nm_val), float(2))
+        int_val = int(math.log(float(nm_val),float(2)))
+    else :
+        int_val = 0
+    #if nm_val <= 0 :
+    #    return 0
+    print(int_val, 8 - int_val)
+    return 8-int_val
+
+def transform_bit(nm):
+    # from digit to integer
+    if nm[0] < 255 :
+        int = decode(nm[0])
+        return int
+    elif nm[0] == 255 and nm[1] < 255:
+        int = decode(nm[1])
+        return 8 + int
+    elif nm[0] == 255 and nm[1] == 255 and nm[2] < 255:
+        int = decode(nm[2])
+        return 16 + int
+    elif nm[0] == 255 and nm[1] == 255 and nm[2] == 255 and nm[3] <= 255:
+        int = decode(nm[3])
+        print("In transform_bit in", nm[3]," out",int)
+        return 24 + int
+    else:
+        return 0 #nm
+    # return bitwise netmask
+
+def nm_input(starting,ending,flag):
     # if flag is True entering a dotted netmask
     # if flag is False entering a bitwise netmask
     # get user value and process it
     if flag :
         netmask = ip_input()
+        bit_netmask = transform_bit(netmask)
     else:
         bit_netmask = int_input("Enter an integer value from 0 to 32 : ") 
-        netmask = transform_bit(bit_netmask)
+    print("ready to calculate final after",flag," we computed",bit_netmask)
     # calculate ending using starting and netmask
-    ending = calculate_final(starting,netmask)
-    return ending
+    calculate_final(starting,ending, int(bit_netmask))
+    return starting, ending
 
-def transform_bit(bit):
-    # from digit to integer
-    return [255,255,255,255] #nm
+def bit(ip,nip,num):
+    # print ("bit " + str(num))
+    powr = int(math.log((ip[3]),2))
+    lor = int(ip[3] - (math.pow(2 , powr)))
+    print ("IP, lor 2 powr ", ip[3], lor, 2, powr)
+    if num == 0 :
+        ip[3] = 0
+        nip[3] = 255
+    elif num == 1 :
+        if ip[3] >= 128:
+            ip[3] = 128
+            nip[3] = 255
+        else:
+            ip[3] = 0
+            nip[3] = 127
+    elif num == 2 :
+        if ip[3] >= 192:
+            ip[3] = 192
+            nip[3] = 255
+        elif ip[3] >= 128 and ip[3] < 192:
+            ip[3] = 128
+            nip[3] = 191
+        elif ip[3] >= 64 and ip[3] < 128:
+            ip[3] = 64
+            nip[3] = 127
+        else:
+            ip[3] = 0
+            nip[3] = 63
+    elif num == 3 :
+        ret_num = 224    
+    elif num == 4 :
+        ret_num = 240    
+    elif num == 4 :
+        ret_num = 248
+    elif num == 5 :
+        ret_num = 252    
+    elif num == 6 :
+        ret_num = 254    
+    elif num == 7 :
+        ret_num = 255 
+    return 
 
-def calculate_final(ip,nm):
+def calculate_final(ip,nip,nm):
+    print (ip, nip, nm)
     # apply netmask on starting to calculate final
-    return ip # end
+    if nm < 24 :
+        print("Too big scan, reduced to /24")
+    else:
+        # bit(ip,nip,nm-24)
+        # print("Calculate final ",ip[3], nip[3])
+        print ("Calculate final ",ip, nip)
+    return 
 
 def user_input():
     # present user the current eth0 address
@@ -79,7 +156,9 @@ def user_input():
     # print("Use dotted IP notation aaa.bbb.ccc.ddd")
     # use tailored input to discriminate good values; return a list of 4 value 0-255
     starting = ip_input()
-    print(starting)
+    valid = input("Are eth0 ", ip_address, " and your input", starting, " on the same LAN ? [N to restart] ")
+    if valid == "N" : 
+        starting = ip_input()
     #
     print("Specify the end of the range in one of the following notation")
     print("1. Dotted IP\n2. Dotted Netmask\n3. Bitwise Netmask")
@@ -95,11 +174,13 @@ def user_input():
             correct = True
         elif choice == 2 :
             print("Selected Dotted Netmask")
-            ending = nm_input(starting,True)
+            ending = starting.copy()
+            nm_input(starting,ending,True)
             correct = True
         elif choice == 3 :
             print("Selected Bitwise Netmask")
-            ending = nm_input(starting,False)
+            ending = starting.copy()
+            nm_input(starting,ending,False)
             correct = True
         else :
             choice = input("Unknown choice select again : ")
