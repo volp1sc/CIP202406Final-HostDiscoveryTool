a38ac5ac6cd0cf95e0a162fbb9fdd30499888070
volp1sc <volp1s.mail@gmail.com>
2024-06-12 19:22:12 +0200

    Created main

---
+++ main.py
@@ -0,0 +1,19 @@
+from user_input import user_input
+from host_discovery_main import get_hosts_answers
+
+def main():
+    #
+    # request user information
+    # return range of hosts to scan
+    #
+    hostmin, hostmax = user_input()
+    #
+    ###DEBUG#print (hostmin, hostmax)
+    #
+    # ready to ping for hosts status
+    #
+    get_hosts_answers(hostmin,hostmax)
+
+
+if __name__ == '__main__':
+    main()
